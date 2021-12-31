59b04e05bc3ffd7338eaf512390a206ca71ba80d
Hudson Kim <hkim@Hudsons-MacBook-Pro.local>
2021-12-31 01:30:09 -0800

    Added sudo code for hand tracking movement control

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -34,8 +34,11 @@ int max = 640;
 
 int i = 0;
 
+// tracking (CAMERA SHOULD BE POINTING TO PERSON, NOT CEILING!)
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(2000000);
+  Serial.setTimeout(1);
   
   pwm.begin();
   pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
@@ -94,11 +97,84 @@ void closeGripper() {
   delay(500);
 }
 
- 
-void loop() { 
-  while(i == 0) {
-    openGripper();
-    closeGripper();
-    i++;
+void open() {
+  openGripper();
+}
+
+void close() {
+  closeGripper();
+}
+
+void right() {
+  Serial.println("RIGHT");
+}
+
+void left() {
+  Serial.println("LEFT");
+}
+
+void position(String x, String y, String z) {
+  
+  // x movement
+  if(x > origin_x + circle_offset) {
+    right(); // motor
   }
+  if(y < origin_x - circle_offset) {
+    left(); // motor
+  }
+  
+  // y movement
+  if(y > origin_y + circle_offset) {
+    up(); // shoulder
+  }
+  if(y < origin_y - circle_offset) {
+    down(); // shoulder
+  }
+
+  // z movement
+  if(z > origin_z + circle_offset) {
+    forward(); // elbow + MAKE WRIST JOIN ONE ALWAYS KEEP GRIPPER LEVEL
+  }
+  if(y < origin_z - circle_offset) {
+    backward(); // elbow + MAKE WRIST JOIN ONE ALWAYS KEEP GRIPPER LEVEL
+  }
+}
+
+void loop() { 
+ while (!Serial.available());
+ input = Serial.readString();
+ identifier = input.substring(0,3);
+ remaining = input.substring(3);
+ 
+ char Buf[50];
+ remaining.toCharArray(Buf, 50);
+
+ // get origin (x,y,z) after delay
+ 
+ if(identifier == "xyz") {
+  x = String(strtok(Buf, ","));
+  y = String(strtok(NULL, ","));
+
+  last = String(strtok(NULL, ","));
+  z = last.substring(0,3);
+  trailing = last.substring(3);
+
+  position(x,y,z);
+
+  if(trailing == "ope") {
+    open();
+  }
+
+  if(trailing == "clo") {
+    close();
+  }
+
+  if(trailing == "rig") {
+    right_wrist(); // rotate wrist
+  }
+
+  if(trailing == "lef") {
+    left_wrist(); // rotate wrist
+  }
+ }
 }
